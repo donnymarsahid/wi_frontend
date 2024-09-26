12038834f5c5e78deb46d77887be10da66828358
callmedonn <donnymrshd.94@gmail.com>
2024-09-26 18:07:11 +0700

    feat(env): adjustment environment

--- src/app/utils/constans.ts
+++ src/app/utils/constans.ts
@@ -1,4 +1,4 @@
-export const STRAPI_URL = process.env.NEXT_PUBLIC_API_URL;
+export const STRAPI_URL = process.env.NEXT_PUBLIC_API_URL || "https://qa-wi.gudangsystem.com";
 export const CRYPTO_SECRET_KEY =
   process.env.CRYPTO_SECRET_KEY || "aSCvrtFZ^sjGashgAneqwf!2@sCVULsui";
 export const IP_URL = process.env.IP_URL_CLIENT || "https://wi.gudangsystem.com";
