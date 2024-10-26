ded4271856fd7529b08ea6269f6406c39168197b
callmedonn <donnymrshd.94@gmail.com>
2024-10-27 01:18:38 +0700

    change format image quality 5

--- src/components/atoms/carouselbanner.tsx
+++ src/components/atoms/carouselbanner.tsx
@@ -120,11 +120,11 @@ export default function CarouselBanner({ homepage }: HeroHomeProps) {
           {homepage.data.attributes.banners.data.map((item, index) => (
             <div key={index}>
               <Image
-                src={`${STRAPI_URL}${item.attributes.url}`}
+                src={`${STRAPI_URL}${item.attributes.formats.medium.url}`}
                 width={1000}
                 height={600}
                 placeholder="blur"
-                blurDataURL={`${STRAPI_URL}${item.attributes.url}?w=30&q=10`} // Placeholder low-res
+                blurDataURL={`${STRAPI_URL}${item.attributes.formats.medium.url}?w=30&q=10`} // Placeholder low-res
                 className="bg-cover bg-center w-full h-full pb-4"
                 alt="banners"
               />
