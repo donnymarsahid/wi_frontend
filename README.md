cea02fee20e946acea4bc455c8ed77b29c6bb8b5
callmedonn <donnymrshd.94@gmail.com>
2024-10-26 15:09:20 +0700

    otw production

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ pnpm dev
 bun dev
 ```
 
-Open [https://wallpaperindonesia.com](https://wallpaperindonesia.com) with your browser to see the result.
+Open [https://www.wallpaperindonesia.com](https://www.wallpaperindonesia.com) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -10,8 +10,7 @@ export async function generateMetadata({ params }: Slug): Promise<Metadata> {
   const products: ProductsProps = await getData({
     path: `products`,
     params: {
-      populate:
-        "seo",
+      populate: "seo",
       "filters[slug][$eq]": params.slug,
     },
   });
@@ -28,8 +27,10 @@ export async function generateMetadata({ params }: Slug): Promise<Metadata> {
     };
   } catch (error) {
     return {
-      title: "Wallpaper Indonesia | Pusat Belanja Online Material Interior di Indonesia",
-      description: "Wallpaper Indonesia merupakan pusat pembelanjaan online untuk material interior mulai dari Wallpaper, Wallfoam 3D, Vinyl Flooring, Parquet Flooring, Gordyn, Blind dan Carpet Tile. Tersedia dalam beragam motif, warna, dan merek yang sesuai dengan kebutuhan Anda.",
+      title:
+        "Wallpaper Indonesia | Pusat Belanja Online Material Interior di Indonesia",
+      description:
+        "Wallpaper Indonesia merupakan pusat pembelanjaan online untuk material interior mulai dari Wallpaper, Wallfoam 3D, Vinyl Flooring, Parquet Flooring, Gordyn, Blind dan Carpet Tile. Tersedia dalam beragam motif, warna, dan merek yang sesuai dengan kebutuhan Anda.",
     };
   }
 }
@@ -55,7 +56,7 @@ export default async function SlugProducts({ params }: Slug) {
 
   return (
     <>
-      <main className="mt-[120px]">
+      <main className="mt-[100px] md:mt-[200px] lg:mt-[100px]">
         <Detail data={products} flashsale={flashsale} />
       </main>
     </>
