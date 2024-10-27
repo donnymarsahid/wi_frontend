94efc61b988079e25564b0203e7c43449be42c11
callmedonn <donnymrshd.94@gmail.com>
2024-10-27 09:42:42 +0700

    change api ip url

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ pnpm dev
 bun dev
 ```
 
-Open [https://www.wallpaperindonesia.com](https://www.wallpaperindonesia.com) with your browser to see the result.
+Open [https://qa.wallpaperindonesia.com](https://qa.wallpaperindonesia.com) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
--- src/app/utils/constans.ts
+++ src/app/utils/constans.ts
@@ -3,6 +3,6 @@ export const STRAPI_URL =
 export const CRYPTO_SECRET_KEY =
   process.env.CRYPTO_SECRET_KEY || "aSCvrtFZ^sjGashgAneqwf!2@sCVULsui";
 export const IP_URL =
-  process.env.IP_URL_CLIENT || "https://www.wallpaperindonesia.com";
+  process.env.IP_URL_CLIENT || "https://qa.wallpaperindonesia.com";
 export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 export const YOUTUBE_CHANNEL = process.env.YOUTUBE_CHANNEL;
