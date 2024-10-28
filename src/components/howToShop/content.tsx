5c1f31ae94aa8487f819765c950f1c64ba043c4d
callmedonn <donnymrshd.94@gmail.com>
2024-10-29 02:17:07 +0700

    change api ip url 2

--- src/components/howToShop/content.tsx
+++ src/components/howToShop/content.tsx
@@ -6,7 +6,6 @@ import cx from "classnames";
 import { poppins } from "@/app/fonts";
 import Clients from "../home/clients";
 import { ClientProps } from "@/types/client";
-import styled, { keyframes, css } from "styled-components";
 import Image from "next/image";
 import { STRAPI_URL } from "@/app/utils/constans";
 import CarouselBannerPortfolio from "../atoms/carouselbannerportfolio";
@@ -53,89 +52,3 @@ export default function Content({ data }: HowToShopDetailProps) {
     </div>
   );
 }
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: fit-content;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-const Text = styled.div`
-  font-size: 35px;
-  font-weight: 500;
-  margin-bottom: 10px;
-  color: #02203c;
-`;
-
-const Note = styled.div`
-  font-size: 18px;
-  font-weight: 200;
-  margin-bottom: 40px;
-  color: #7c8e9a;
-`;
-
-const Marquee = styled.div`
-  display: flex;
-  width: 100%;
-  overflow: hidden;
-  user-select: none;
-
-  mask-image: linear-gradient(
-    to right,
-    hsl(0 0% 0% / 0),
-    hsl(0 0% 0% / 1) 10%,
-    hsl(0 0% 0% / 1) 90%,
-    hsl(0 0% 0% / 0)
-  );
-`;
-
-const scrollX = keyframes`
-  from {
-    left: translateX(0);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-`;
-
-const common = css`
-  flex-shrink: 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  white-space: nowrap;
-  width: 100%;
-  animation: ${scrollX} 30s linear infinite;
-`;
-
-const MarqueeGroup = styled.div`
-  ${common}
-`;
-const MarqueeGroup2 = styled.div`
-  ${common}
-  animation-direction: reverse;
-  animation-delay: -3s;
-`;
-
-const ImageGroup = styled.div`
-  display: grid;
-  place-items: center;
-  width: clamp(10rem, 1rem + 40vmin, 30rem);
-  padding: 0;
-  margin-right: 5px;
-  margin-left: 5px;
-`;
-
-// const Image = styled.img`
-//   object-fit: contain;
-//   width: 100%;
-//   height: 100%;
-//   /* border: 1px solid black; */
-//   border-radius: 0.5rem;
-//   aspect-ratio: 16/9;
-//   padding: 5px 20px;
-// `;
