266c925690104cf4778643fdd615d5fabc5db147
callmedonn <donnymrshd.94@gmail.com>
2024-10-29 00:30:31 +0700

    change api ip url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   images: {
-    domains: ["dev.wallpaperindonesia.com", "i.ytimg.com"], // Anda tidak perlu menduplikasi protokol
+    domains: ["panel.wallpaperindonesia.com", "i.ytimg.com"], // Anda tidak perlu menduplikasi protokol
     formats: ["image/avif", "image/webp"], // Mengatur format gambar yang optimal
   },
 };
