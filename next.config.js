f25d687f602e918186b9d9b94ac2eb31aca80d6a
callmedonn <donnymrshd.94@gmail.com>
2025-02-12 16:52:00 +0700

    feat: progress fix deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const nextConfig = {
+  reactStrictMode: false,
   images: {
     domains: ["panel.wallpaperindonesia.com", "i.ytimg.com"], // Anda tidak perlu menduplikasi protokol
     formats: ["image/avif", "image/webp"], // Mengatur format gambar yang optimal
