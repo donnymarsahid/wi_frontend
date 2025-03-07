6d101fcc4b4fe546abce4e1353351b778efc052d
callmedonn <donnymrshd.94@gmail.com>
2025-03-08 01:41:14 +0700

    feat: refactoring done

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -13,7 +13,7 @@ export default function Loading() {
   let [color, setColor] = useState("#45BDDB");
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <main className="flex h-screen w-full items-center justify-center absolute top-0 bottom-0 bg-white z-[99999]">
+    <div className="flex h-screen w-screen items-center justify-center absolute top-0 bottom-0 right-0 left-0 bg-white z-[9999999999]">
       <div>
         <BeatLoader
           color={color}
@@ -24,6 +24,6 @@ export default function Loading() {
           data-testid="loader"
         />
       </div>
-    </main>
+    </div>
   );
 }
