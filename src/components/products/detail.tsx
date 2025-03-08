9cbcc3010d97e0a5855bd8ff824cf58edca35eb3
callmedonn <donnymrshd.94@gmail.com>
2025-03-08 11:55:18 +0700

    feat: refactoring done

--- src/components/products/detail.tsx
+++ src/components/products/detail.tsx
@@ -663,7 +663,7 @@ export default function Detail({ data, flashsale }: ProductPageProps) {
                                         </div>
                                         <div className="text-[14.5px]">
                                           <div>
-                                            <p className="md:text-[14.5px] text-[9.5px] font-medium text-[#474747]">
+                                            <p className="md:text-[17.5px] text-[12.5px] font-bold">
                                               {!item?.attributes?.brands
                                                 ?.data[0]?.attributes
                                                 ?.discount &&
@@ -697,7 +697,7 @@ export default function Detail({ data, flashsale }: ProductPageProps) {
                                                   : 0
                                               )}{" "}
                                               /{" "}
-                                              <span className="capitalize md:text-[14.5px] text-[9.5px]">
+                                              <span className="capitalize md:text-[17.5px] text-[12.5px] font-bold">
                                                 {item?.attributes?.brands
                                                   ?.data[0]?.attributes?.unit
                                                   ? String(
