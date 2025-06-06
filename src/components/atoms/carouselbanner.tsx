b927e8cdd42768498badb40cc046e62a7df9864b
callmedonn <donnymrshd.94@gmail.com>
2025-06-06 14:41:35 +0700

    feat: preparation build --no-lint

--- src/components/atoms/carouselbanner.tsx
+++ src/components/atoms/carouselbanner.tsx
@@ -122,7 +122,7 @@ export default function CarouselBanner({ homepage }: HeroHomeProps) {
               <Image
                 src={`${STRAPI_URL}${item.attributes.url}`}
                 width={1000}
-                height={600}
+                height={150}
                 placeholder="blur"
                 blurDataURL={`${STRAPI_URL}${item.attributes.url}?w=30&q=10`} // Placeholder low-res
                 className="bg-cover bg-center w-full h-full pb-4"
