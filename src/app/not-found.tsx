e041aed0f18acf18f0f6fcbc697817180c2b7105
callmedonn <donnymrshd.94@gmail.com>
2025-07-09 23:19:41 +0700

    feat: fix SEM

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import cx from "classnames";
 import { poppins } from "./fonts";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/category/wallpaper--Wallpaper");
+  }, [router]);
+
   return (
     <div
       className={`flex min-h-screen flex-col items-center justify-center py-6 ${cx(
