3c93fe7deaee4c1b28fd6d6ae8f598eb62aadf1e
callmedonn <donnymrshd.94@gmail.com>
2025-07-11 20:02:59 +0700

    feat: fix SEM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,10 +92,10 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/favicon.ico" />
-        {/* <meta
+        <meta
           name="google-site-verification"
           content="O22Z-eY2MtqN8TftUUf7o5_wmtfTMdeaSjMsRZU4Xd4"
-        /> */}
+        />
       </head>
       <body suppressHydrationWarning={true}>
         {/* GTM NoScript */}
