c217885b3acf688f87114e13967f3fd156308250
callmedonn <donnymrshd.94@gmail.com>
2025-07-15 20:49:14 +0700

    feat: fix SEM

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["panel.wallpaperindonesia.com", "i.ytimg.com"], // Anda tidak perlu menduplikasi protokol
+    domains: ["panel.wallpaperindonesia.co.id", "i.ytimg.com"], // Anda tidak perlu menduplikasi protokol
     formats: ["image/avif", "image/webp"], // Mengatur format gambar yang optimal
   },
 };
