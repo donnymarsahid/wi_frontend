bacf0e8f3293ea70258bd3e77018e64d551940be
callmedonn <donnymrshd.94@gmail.com>
2025-07-17 12:02:30 +0700

    feat: change co.id to com

--- src/app/utils/constans.ts
+++ src/app/utils/constans.ts
@@ -1,5 +1,5 @@
 export const STRAPI_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://panel.wallpaperindonesia.co.id";
+  process.env.NEXT_PUBLIC_API_URL || "https://panel.wallpaperindonesia.com";
 export const CRYPTO_SECRET_KEY =
   process.env.CRYPTO_SECRET_KEY || "aSCvrtFZ^sjGashgAneqwf!2@sCVULsui";
 export const IP_URL = process.env.IP_URL_CLIENT || "http://localhost:3000";
