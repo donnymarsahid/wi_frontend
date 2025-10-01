9322b6f8bb5433989dd27bc110d1eb7938616c79
callmedonn <donnymrshd.94@gmail.com>
2025-10-01 15:30:29 +0700

    feat: change font to worksans

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -1,6 +1,8 @@
-import { Poppins } from "next/font/google";
+import { Work_Sans } from "next/font/google";
+// import { Poppins } from "next/font/google";
 
-export const poppins = Poppins({
+export const poppins = Work_Sans({
+  // export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
   variable: "--font-poppins",
