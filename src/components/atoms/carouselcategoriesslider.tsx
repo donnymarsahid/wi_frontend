ebfe87698516f784816b3e5330ddd14ffefe7fa5
callmedonn <donnymrshd.94@gmail.com>
2025-10-02 15:54:19 +0700

    feat: Penyesuaian UI pada style lantai vinyl bagian card category

--- src/components/atoms/carouselcategoriesslider.tsx
+++ src/components/atoms/carouselcategoriesslider.tsx
@@ -33,178 +33,6 @@ const responsive = {
   },
 };
 
-const CustomDot = ({ onClick, active }: any) => {
-  return (
-    <li
-      className={`dot ${
-        active ? "active rounded-lg w-[40px]" : "inactive rounded-full w-[10px]"
-      }`}
-      onClick={onClick}
-    />
-  );
-};
-
-// Add some styles to achieve the look from the image
-const styles = `
-.carousel-container {
-  position: relative;
-}
-
-.dot {
-  display: inline-block;
-  height: 10px;
-  margin-right: 5px;
-  cursor: pointer;
-}
-
-.dot.inactive {
-  border: 1px solid #5BC0DE; /* Color for the inactive dots (outline) */
-  background-color: transparent;
-}
-
-.dot.active {
-  background-color: #5BC0DE; /* Color for the active dot (filled) */
-}
-
-.container-with-dots {
-  position: relative;
-}
-
-.container-with-dots .react-multi-carousel-dot-list {
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-`;
-
-// Versi Slider
-// export default function CarouselCategoriesSlider({
-//   categories,
-// }: HeroHomeProps) {
-//   return (
-//     <>
-//       <style>{styles}</style>
-//       <div className="carousel-container">
-//         <Carousel
-//           additionalTransfrom={0}
-//           arrows={true}
-//           centerMode={false}
-//           className=""
-//           containerClass="container-with-dots"
-//           dotListClass=""
-//           draggable={true}
-//           focusOnSelect={false}
-//           infinite
-//           itemClass=""
-//           keyBoardControl
-//           minimumTouchDrag={80}
-//           pauseOnHover
-//           customDot={<CustomDot />}
-//           renderArrowsWhenDisabled={false}
-//           renderButtonGroupOutside={false}
-//           renderDotsOutside={false}
-//           responsive={responsive}
-//           rewind={false}
-//           rewindWithAnimation={false}
-//           rtl={false}
-//           shouldResetAutoplay
-//           showDots={false}
-//           sliderClass=""
-//           slidesToSlide={1}
-//           swipeable
-//         >
-//           {categories
-//             .sort(
-//               (a, b) =>
-//                 new Date(b.attributes.date).getTime() -
-//                 new Date(a.attributes.date).getTime()
-//             )
-//             .map((item, index) => (
-//               <Link
-//                 href={`#${item.attributes.name}`}
-//                 className="mt-2"
-//                 key={index}
-//               >
-//                 <div
-//                   key={index}
-//                   className="relative mt-4 overflow-hidden cursor-pointer rounded-lg md:h-[98px] h-[60px] me-4"
-//                 >
-//                   {item.attributes?.thumbnail?.data?.attributes?.url && (
-//                     <Image
-//                       src={`${STRAPI_URL}${item.attributes?.thumbnail?.data?.attributes?.url}`}
-//                       width={400}
-//                       height={400}
-//                       alt="wall"
-//                       className="w-full h-full object-cover transform transition-transform duration-500 hover:scale-110 rounded-lg"
-//                     />
-//                   )}
-//                 </div>
-//                 <div>
-//                   <div className="w-full mb-4">
-//                     <div
-//                       className={`relative mt-[-20px] flex justify-center ${cx(
-//                         poppins,
-//                         poppins.className
-//                       )}`}
-//                     >
-//                       <h3 className="py-2 px-4 bg-white bg-opacity-90 rounded-2xl lg:text-[16px] text-[10px] text-center shadow-lg">
-//                         {item.attributes.name.replace("Lantai", "")}
-//                       </h3>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </Link>
-//             ))}
-//           {categories
-//             .sort(
-//               (a, b) =>
-//                 new Date(b.attributes.date).getTime() -
-//                 new Date(a.attributes.date).getTime()
-//             )
-//             .map((item, index) => (
-//               <Link href={""} className="mt-2" key={index}>
-//                 <div
-//                   key={index}
-//                   className="relative mt-4 overflow-hidden cursor-pointer rounded-lg md:h-[98px] h-[60px] me-4"
-//                 >
-//                   {item.attributes?.thumbnail?.data?.attributes?.url && (
-//                     <Image
-//                       src={`${STRAPI_URL}${item.attributes?.thumbnail?.data?.attributes?.url}`}
-//                       width={400}
-//                       height={400}
-//                       alt="wall"
-//                       className="w-full h-full object-cover transform transition-transform duration-500 hover:scale-110 rounded-lg"
-//                     />
-//                   )}
-//                 </div>
-//                 <div>
-//                   <div className="w-full">
-//                     <div
-//                       className={`relative mt-[-20px] flex justify-center ${cx(
-//                         poppins,
-//                         poppins.className
-//                       )}`}
-//                     >
-//                       <h3 className="py-2 px-4 bg-white bg-opacity-90 rounded-2xl lg:text-[16px] text-[10px] text-center shadow-lg">
-//                         {item.attributes.name.replace("Lantai", "")}
-//                       </h3>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </Link>
-//             ))}
-//         </Carousel>
-//       </div>
-//     </>
-//   );
-// }
-
 // Versi Tidak Slider
 export default function CarouselCategoriesSlider({
   categories,
@@ -226,7 +54,7 @@ export default function CarouselCategoriesSlider({
             >
               <div
                 key={index}
-                className="relative overflow-hidden cursor-pointer rounded-lg md:h-[98px] h-[60px] me-4 w-full"
+                className="overflow-hidden cursor-pointer rounded-t-lg md:h-[80px] h-[50px] me-4 w-full"
               >
                 {item.attributes?.thumbnail?.data?.attributes?.url && (
                   <Image
@@ -234,19 +62,19 @@ export default function CarouselCategoriesSlider({
                     width={400}
                     height={400}
                     alt="wall"
-                    className="w-full h-full object-cover transform transition-transform duration-500 hover:scale-110 rounded-lg"
+                    className="w-full h-full object-cover transform transition-transform duration-500 hover:scale-110 rounded-t-lg border-s-[1.2px] border-e-[1.2px] border-t-[1.2px] border-[#201E1C]"
                   />
                 )}
               </div>
               <div>
-                <div className="w-full">
+                <div className="w-full rounded-b-lg border-s-[1.2px] border-e-[1.2px] border-b-[1.2px] border-[#201E1C]">
                   <div
-                    className={`relative mt-[-20px] flex justify-center ${cx(
+                    className={`flex justify-center ${cx(
                       poppins,
                       poppins.className
                     )}`}
                   >
-                    <h3 className="py-2 px-4 bg-white bg-opacity-90 rounded-2xl lg:text-[15px] text-[10px] text-center shadow-lg">
+                    <h3 className="py-1 px-3 bg-white  lg:text-[15px] text-[8.5px] text-center rounded-lg">
                       {item.attributes.name.replace("Lantai", "")}
                     </h3>
                   </div>
