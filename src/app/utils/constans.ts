76076b214f9339b84687eff7fabcb0c225484145
callmedonn <donnymrshd.94@gmail.com>
2025-10-04 01:54:47 +0700

    feat: change IP URL CLIENT

--- src/app/utils/constans.ts
+++ src/app/utils/constans.ts
@@ -2,6 +2,7 @@ export const STRAPI_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://panel.wallpaperindonesia.com";
 export const CRYPTO_SECRET_KEY =
   process.env.CRYPTO_SECRET_KEY || "aSCvrtFZ^sjGashgAneqwf!2@sCVULsui";
-export const IP_URL = process.env.IP_URL_CLIENT || "http://localhost:3000";
+export const IP_URL =
+  process.env.IP_URL_CLIENT || "https://www.wallpaperindonesia.com";
 export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 export const YOUTUBE_CHANNEL = process.env.YOUTUBE_CHANNEL;
