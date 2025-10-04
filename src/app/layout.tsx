6433d056f3fbcfe85b6ba0b3d4e21a7afcc0506b
callmedonn <donnymrshd.94@gmail.com>
2025-10-04 10:20:19 +0700

    feat: embed gtag and ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,6 +112,30 @@ export default async function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-W8ZTLQL8'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W8ZTLQL8');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KMCYHQV6GM"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-KMCYHQV6GM');
+  `}
+        </Script>
       </head>
       <body suppressHydrationWarning={true}>
         {/* GTM NoScript */}
@@ -135,6 +159,16 @@ export default async function RootLayout({
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1280292583433659&ev=PageView&noscript=1"
           />
+        </noscript>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W8ZTLQL8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
         </noscript>
 
         <UserProvider>
